798605e17d3d5b58ed39e0bd1cf73e0b866570a9
zosky <boolioncube@gmail.com>
2022-11-28 23:51:06 -0500

    Initial commit

---
+++ vite.config.ts
@@ -0,0 +1,21 @@
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import AutoImport from 'unplugin-auto-import/vite'
+import VueRouter from 'unplugin-vue-router/vite'
+import { VueRouterAutoImports } from 'unplugin-vue-router'
+import Components from 'unplugin-vue-components/vite'
+import { HeadlessUiResolver } from 'unplugin-vue-components/resolvers'
+import eslintPlugin from 'vite-plugin-eslint'
+
+export default defineConfig({
+  server: { open: true },
+  plugins: [
+    VueRouter({}),
+    vue(),
+    Components({ resolvers: [HeadlessUiResolver()] }),
+    AutoImport({
+      imports: ['vue', '@vueuse/head', VueRouterAutoImports],
+    }),
+    eslintPlugin()
+  ],
+})
