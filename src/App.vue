8cb41e829992a363a4913bbe1745afd7f4e32860
marc bulbulian <boolioncube@gmail.com>
2022-11-30 12:36:43 -0500

    global app layout
    nav(s) + router-view + bkgBocca

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,13 @@
 <script setup>
-let counter = ref(0)
-
-setInterval(() => {
-  counter.value++
-}, 1000)
 </script>
 
 <template>
   <div>
-    <header v-if="$route.meta.title" class="bg-white shadow">
-      <div class="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
-        <h1
-          class="text-3xl font-bold leading-tight text-gray-900"
-          title="click to reset a counter"
-          @click="counter = 0">
-          {{ $route.meta.title }} / {{ counter }}
-        </h1>
-      </div>
-    </header>
+    <TopNav />
+    <NavPersonPages v-if="$route?.path!='/'" class="z-10"/>
     <main>
       <router-view />
     </main>
+    <BackgroundBocca />
   </div>
 </template>
