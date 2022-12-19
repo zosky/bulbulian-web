3bff19f3266d5a74bed375ed04a20b94b94c361f
marc bulbulian <boolioncube@gmail.com>
2022-12-18 23:02:29 -0500

    no related vids on youTube embed

--- src/components/YouTube.vue
+++ src/components/YouTube.vue
@@ -4,7 +4,7 @@ defineProps({id:{type:String,default:'Y1JWQ8_ms5U'}})
 
 <template>
   <iframe
-    class="aspect-[16/9]" :src="`https://www.youtube.com/embed/${id}`"
+    class="aspect-[16/9]" :src="`https://www.youtube.com/embed/${id}?rel=0`"
     frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
     allowfullscreen></iframe>  
 </template>
